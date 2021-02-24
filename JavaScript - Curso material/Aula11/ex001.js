4596c1c21191e6fc9b0da77cbf9240346e8a1006
Joel Pedro J. da Silva <joel.pjds@gmail.com>
2021-02-23 23:04:02 -0300

    Adicionais

---
+++ JavaScript - Curso material/Aula11/ex001.js
@@ -0,0 +1,7 @@
+var velo = 40
+
+console.log (`A velocidade do seu carro é ${velo}!`)
+if (velo > 60){
+    console.log (`Você ultrapassou a velocidade permitida ! MUTADO.`)
+}
+console.log (`Dirija sempre com segurança!`)
