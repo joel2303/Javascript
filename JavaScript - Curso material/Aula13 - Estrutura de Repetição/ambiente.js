b8ec24b297ab251662d1d92a3a1122ff4bf184e5
Joel Pedro J. da Silva <joel.pjds@gmail.com>
2021-03-01 22:02:26 -0300

    Exercicio pratico JS

---
+++ JavaScript - Curso material/Aula13 - Estrutura de Repetição/ambiente.js
@@ -0,0 +1,24 @@
+var c = 20
+console.log  ("Contando até 50: ")
+do {
+    console.log(`Passo: ${c}`)
+    c-- // c++ = c + 1
+} while (c > 0)
+
+/*
+var c = 1
+while (c <= 6){
+    console.log(`Passo: ${c}`)
+    c++ // c++ = c + 1
+}
+*/
+
+
+/*
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+*/
---
+++ JavaScript - Curso material/Aula13 - Estrutura de Repetição/ex002.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <= 1){
+    console.log (c)
+    c ++
+}   
+*/
+console.log ("Vai começar: ")
+for (var c = 1;c <= 10; c++){
+    console.log(`Passo: ${c}`)
+}
+console.log("FIM !")
+
