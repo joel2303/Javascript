cb38d24d1f3e7a26965bd06f91c8b4413139b3d8
Joel Pedro J. da Silva <joel.pjds@gmail.com>
2021-03-14 22:43:30 -0300

    Exercicio simples

---
+++ JavaScript - Curso material/exercicios-pratica/ex7/script.js
@@ -0,0 +1,43 @@
+function contar (){
+    var inicio = document.getElementById ("inicio")
+    var passo = document.getElementById ("passo")
+    var fim = document.getElementById ("fim")
+    var resul2 = document.getElementById ("resul2")
+    var resul = document.getElementById ("resul")
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        resul2.innerHTML = "Por favor,<br><br> preencha todos <br><br>os dados corretamente!"
+        resul.innerHTML = "Impossivel contar."
+    } else {
+        resul2.innerHTML = "Tudo correto,<br><br> o resultado irá aparecer abaixo!"
+        resul.innerHTML = "Contando: "
+
+        var i = Number (inicio.value)
+        var f = Number (fim.value)
+        var p = Number (passo.value)
+
+        if (i < f) {
+            // Contagem crescente
+            for (var c = i ; c <= f ; c = c + p) {
+            resul.innerHTML += `${c} \u{2763} `
+            }
+
+        } else {
+            // Contagem regressiva
+            for (var c = i ; c >= f ; c = c - p){
+            resul.innerHTML += `${c} \u{2763} `
+            }
+        }
+    resul.innerHTML += `\u{1F3C1}`
+    }
+        
+    }
+
+function resetar(){
+    var resul2 = document.getElementById ("resul2")
+    var resul = document.getElementById ("resul")
+
+    resul2.innerHTML = "Preencha os <br><br> dados ao lado <br><br> para dar inicio <br><br> a contagem !"
+    resul.innerHTML = "Preparando contagem ..."
+
+}
